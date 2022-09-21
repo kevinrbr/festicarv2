765265d64df2e0f9bdd2157755256c14c714c9ad
Kevin <kevinrbr16@gmail.com>
2022-09-22 00:49:33 +0200

    banner component

---
+++ components/Banner.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import bannerImage from "./../assets/images/escales-banner.png";
+
+export const Banner = () => {
+  return (
+    <div className="h-[420px] relative bg-primary-200">
+      {/* <Image
+        src={bannerImage}
+        height="420px"
+        width="100%"
+        layout="fill"
+        objectFit="cover" */}
+    </div>
+  );
+};
