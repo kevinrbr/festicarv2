dbab15ecbf59bb6075035e24fb95957dba14f195
Kevin <kevinrbr16@gmail.com>
2022-09-26 18:20:54 +0200

    adding react hooks form

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 import { useRouter } from "next/router";
 import supabase from "../utils/supabase";
@@ -7,23 +8,26 @@ import Link from "next/link";
 
 import { ArrowLongLeftIcon } from "@heroicons/react/24/outline";
 
+interface IFormInput {
+  firstName: String;
+  lastName: String;
+  email: string;
+  password: string;
+}
+
 const SignUp: NextPage = () => {
   const router = useRouter();
-  const [prenom, setPrenom] = useState("");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    console.log(data);
 
     const { user, error } = await supabase.auth.signUp({
-      email,
-      password,
+      email: data.email,
+      password: data.password,
     });
 
     if (user) {
-      updateProfile(user.id);
+      updateProfile(user.id, data.firstName);
     }
 
     if (error) {
@@ -33,10 +37,10 @@ const SignUp: NextPage = () => {
     }
   };
 
-  const updateProfile = async (userId: string) => {
+  const updateProfile = async (userId: string, data: String) => {
     const updates = {
       user_id: userId,
-      name,
+      name: data,
     };
 
     const { error } = await supabase.from("profiles").upsert(updates, {
@@ -62,63 +66,43 @@ const SignUp: NextPage = () => {
         <h1 className="text-3xl font-semibold text-center text-white">
           Créer un nouveau compte
         </h1>
-
-        <form className="mt-2 flex flex-col p-6" onSubmit={handleSubmit}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mt-2 flex flex-col p-6"
+        >
           <div className="flex">
             <div className="w-1/2 mr-7">
-              <label htmlFor="prenom" className="text-gray-200">
-                Prenom
-              </label>
+              <label className="text-gray-200">Prenom</label>
               <input
+                {...register("firstName")}
                 className="py-2 px-4 rounded-md focus:outline-none focus:ring-2 w-full"
-                type="prenom"
-                id="prenom"
-                value={prenom}
-                onChange={(e) => setPrenom(e.target.value)}
               />
             </div>
             <div className="w-1/2">
-              <label htmlFor="nom" className="mt-6 text-gray-200">
-                Nom
-              </label>
+              <label className="text-gray-200">Nom</label>
               <input
+                {...register("lastName")}
                 className="py-2 px-4 rounded-md focus:outline-none focus:ring-2 w-full"
-                type="name"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
               />
             </div>
           </div>
 
-          <label htmlFor="email" className="mt-6 text-gray-200">
-            Email
-          </label>
+          <label className="text-gray-200 mt-6">Email</label>
           <input
-            className="py-2 px-4 rounded-md focus:outline-none focus:ring-2"
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            {...register("email")}
+            className="py-2 px-4 rounded-md focus:outline-none focus:ring-2 w-full"
           />
 
-          <label htmlFor="password" className="mt-6 text-gray-200">
-            Mot de passe
-          </label>
+          <label className="text-gray-200 mt-6">Mot de passe</label>
           <input
-            className="py-2 px-4 rounded-md focus:outline-none focus:ring-2"
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            {...register("password")}
+            className="py-2 px-4 rounded-md focus:outline-none focus:ring-2 w-full"
           />
 
-          <button
-            className="mt-10 text-lg text-white font-semibold bg-green-500 py-3 px-6 rounded-md focus:outline-none focus:ring-2"
+          <input
             type="submit"
-          >
-            S'enregistrer
-          </button>
+            className="mt-10 text-lg text-white font-semibold bg-green-500 py-3 px-6 rounded-md focus:outline-none focus:ring-2"
+          />
           <Link href="/signin">
             <a className="text-white text-center text-sm">
               J'ai déjà un compte
